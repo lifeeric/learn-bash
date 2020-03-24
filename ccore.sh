db3ae93733430b03a59b515d255056a5086e5916
LIFE <dkrshw65@gmail.com>
2020-03-24 11:13:48 -0400

    removed

--- ccore.sh
+++ ccore.sh
@@ -2,7 +2,9 @@
 
 CPU_CORES=$(grep -c name /proc/cpuinfo)
 
-if [ $CPU_CORES -lt 4 ] ; then
+if (( $CPU_CORES < 6 )) ; then
 	echo "Minimun 4 cores required!"
+else
+	echo "Your CPU is OKAY!"
 fi
 exit 0
